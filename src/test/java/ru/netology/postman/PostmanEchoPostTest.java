f2017a1b4fb8065e52d7fa9d968cd2a247103e98
Marina Oliynyk <marina.shpak@gmail.com>
2021-05-22 22:54:24 +0400

    Added project and test

---
+++ src/test/java/ru/netology/postman/PostmanEchoPostTest.java
@@ -0,0 +1,38 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoPostTest {
+    @Test
+    void shouldPostData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldPostMessage() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("You made a POST request with the following data!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("You made a POST request with the following data!"))
+        ;
+    }
+}
